3e35183134319adfd3d4b5345c5f702f53a29b3d
SimonBurdy <simon.burdy@lacatholille.fr>
2022-05-28 23:37:51 +0200

    profil image in progress

--- app/src/main/java/com/example/myapplication/ui/feature/profile/ProfileScreen.kt
+++ app/src/main/java/com/example/myapplication/ui/feature/profile/ProfileScreen.kt
@@ -8,21 +8,33 @@ import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.result.launch
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+
+import coil.request.ImageRequest
+import com.example.myapplication.R
 import com.example.myapplication.model.User
 import com.example.myapplication.ui.NavigationKeys
 import com.example.myapplication.ui.feature.teams.TeamsContract
@@ -37,6 +49,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import java.io.ByteArrayOutputStream
 
 @ExperimentalPermissionsApi
 @Composable
@@ -49,33 +62,14 @@ fun ProfileScreen(
         scaffoldState = scaffoldState,
     )
     {
-
         displayProfilInfos(state.user)
-        /*when (val state = viewModel.state.collectAsState().value) {
-            is State.Loading ->
-                LoadingBar()
-            is State.Failed -> {
-                LaunchedEffect(scaffoldState.snackbarHostState) {
-                    scaffoldState.snackbarHostState.showSnackbar(
-                        "Rien à afficher",
-                        duration = SnackbarDuration.Long
-                    )
-                }
-            }
-            is State.Success -> Column {
-                Text(text = state.data.toString())
-                PostCreator {
-                    viewModel.addPost(it)
-                }
-            }
-        }
-    }*/
-        //FeatureThatRequiresCameraPermission()
+
     }
 }
 
 
 
+@OptIn(ExperimentalPermissionsApi::class)
 @Composable
 private fun displayProfilInfos(item : User?){
     val name = remember  { mutableStateOf(TextFieldValue()) }
@@ -83,59 +77,85 @@ private fun displayProfilInfos(item : User?){
     Log.d("USER NAME" , item?.name.toString())
     Column( modifier = Modifier
         .fillMaxWidth()
-        .padding(16.dp),
-        verticalArrangement = Arrangement.Center) {
+        .padding(16.dp).verticalScroll(rememberScrollState())) {
 
-        Text(text = buildAnnotatedString {
-            append("Profil")
-        }, fontSize = 30.sp)
+        Card(
+            shape = CircleShape,
+            modifier = Modifier
+                .padding(8.dp)
+                .size(100.dp)
 
-        Spacer(modifier = Modifier.padding(8.dp))
-        Text(text = "Email actuel : "+(item?.email ?: " Aucun mail actuel") )
+        ) {
+            Image(
+                painterResource(R.drawable.ic_launcher_background),
+                contentDescription = "",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.fillMaxSize()
+            )
+          /*  AsyncImage(
+                model = "https://wallpaperaccess.com/full/137507.jpg" *//*ImageRequest.Builder(LocalContext.current)
+                    .data("https://wallpaperaccess.com/full/137507.jpg")
+                    .crossfade(true)
+                    .build()*//*,
+                placeholder = painterResource(R.drawable.ic_launcher_foreground),
+                contentDescription = "img desc",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .size(64.dp)
+                    .clip(CircleShape)
+            )*/
+        }
+        Column(   modifier = Modifier
+            .padding(20.dp)
+            .background(Color.White)
+            .fillMaxWidth(),
+            horizontalAlignment = Alignment.Start){
+            Spacer(modifier = Modifier.padding(8.dp))
+            Text(text = "Email actuel : "+(item?.email ?: " Aucun mail actuel") )
 
-        Spacer(modifier = Modifier.padding(8.dp))
-        Text(text = "Nom actuel : "+(item?.name ?: " Aucun nom actuel") )
+            Spacer(modifier = Modifier.padding(8.dp))
+            Text(text = "Nom actuel : "+(item?.name ?: " Aucun nom actuel") )
 
-        OutlinedTextField(
-            value = name.value,
-            onValueChange = {
-               if (nameErrorState.value) {
-                    nameErrorState.value = false
-                }
-                name.value = it
-                Log.d("PROFIL SCREEN" , item?.name.toString())
-            },
-            isError = nameErrorState.value,
-            modifier = Modifier.fillMaxWidth(),
-            label = {
-                Text(text = "Nom")
-            },
-        )
 
-        Spacer(modifier = Modifier.padding(8.dp))
-        Button(
-            onClick = {
-                updateProfile(name.value.text)
+            Spacer(modifier = Modifier.padding(8.dp))
+            OutlinedTextField(
+                value = name.value,
+                onValueChange = {
+                    if (nameErrorState.value) {
+                        nameErrorState.value = false
+                    }
+                    name.value = it
+                    Log.d("PROFIL SCREEN" , item?.name.toString())
+                },
+                isError = nameErrorState.value,
+                modifier = Modifier.fillMaxWidth(),
+                label = {
+                    Text(text = "Nom")
+                },
+            )
+            Spacer(modifier = Modifier.padding(8.dp))
+        }
+        Column{
+            Spacer(modifier = Modifier.padding(8.dp))
+            Button(
+                onClick = {
+                    updateProfile(name.value.text)
 
-            },
-            content = {
-                Text(text = "Updater le profil")
-            },
-            modifier = Modifier.fillMaxWidth(),
-        )
-        
-        Button(
-            onClick = {
-                updateProfile(name.value.text)
+                },
+                content = {
+                    Text(text = "Updater le profil")
+                },
+                modifier = Modifier.fillMaxWidth(),
+            )
+            Spacer(modifier = Modifier.padding(8.dp))
 
-            },
-            content = {
-                Text(text = "Updater le profil")
-            },
-            modifier = Modifier.fillMaxWidth(),
-        )
+            FeatureThatRequiresCameraPermission()
+
+        }
     }
+
 }
+
 private fun updateProfile( name : String ) {
     Log.d("NAME" , name);
     val auth = Firebase.auth
@@ -175,8 +195,8 @@ private fun FeatureThatRequiresCameraPermission() {
         // If the camera permission is granted, then show screen with the feature enabled
         PermissionStatus.Granted -> {
             if (bitmapFromCamera.value == null) {
-                Button(onClick = { launcher.launch() }) {
-                    Text("Take a picture")
+                Button(onClick = { launcher.launch() } ,modifier = Modifier.fillMaxWidth()) {
+                    Text("Prendre une photo")
                 }
             } else {
                 bitmapFromCamera.let {
@@ -189,6 +209,17 @@ private fun FeatureThatRequiresCameraPermission() {
                         )
                     }
                 }
+
+
+              /*  val data = bitmapFromCamera.value!!.asImageBitmap()
+
+               // var uploadTask = mountainsRef.putBytes(data)
+                data.addOnFailureListener {
+                    // Handle unsuccessful uploads
+                }.addOnSuccessListener { taskSnapshot ->
+                    // taskSnapshot.metadata contains file metadata such as size, content-type, etc.
+                    // ...
+                }*/
             }
         }
         is PermissionStatus.Denied -> {
