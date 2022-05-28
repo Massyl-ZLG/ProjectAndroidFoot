017a4ba967ebbcf2518f1399761dce77a3bc9b37
SimonBurdy <simon.burdy@lacatholille.fr>
2022-05-28 17:09:36 +0200

    connexion / enregistrement Ok

--- app/src/main/java/com/example/myapplication/ui/EntryPointActivity.kt
+++ app/src/main/java/com/example/myapplication/ui/EntryPointActivity.kt
@@ -6,13 +6,29 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.material.Scaffold
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavHostController
@@ -23,9 +39,11 @@ import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
 import coil.annotation.ExperimentalCoilApi
 import com.example.myapplication.LoginActivity
+import com.example.myapplication.MainActivity
 import com.example.myapplication.ui.NavigationKeys.Arg.MATCH_ID
 import com.example.myapplication.ui.NavigationKeys.Arg.PLAYER_ID
 import com.example.myapplication.ui.NavigationKeys.Arg.TEAM_ID
+import com.example.myapplication.ui.feature.auth.RegisterScreen
 import com.example.myapplication.ui.feature.match_details.MatchDetailsScreen
 import com.example.myapplication.ui.feature.match_details.MatchDetailsViewModel
 import com.example.myapplication.ui.feature.matches.MatchesScreen
@@ -45,6 +63,8 @@ import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.receiveAsFlow
 
@@ -60,6 +80,7 @@ class EntryPointActivity : AppCompatActivity() {
         setContent {
             ComposeSampleTheme {
                 TeamsApp()
+                //FormContainer(this)
             }
         }
     }
@@ -69,37 +90,6 @@ class EntryPointActivity : AppCompatActivity() {
         startActivity(intent)
     }
 
-   /* private fun signIn(){
-        val providers = arrayListOf(
-            AuthUI.IdpConfig.EmailBuilder().build()
-        )
-
-        val  signinIntent = AuthUI.getInstance()
-            .createSignInIntentBuilder()
-            .setAvailableProviders(providers)
-            .build()
-
-        signInLauncher.launch(signinIntent)
-    }
-
-
-
-    private val signInLauncher = registerForActivityResult (
-        FirebaseAuthUIActivityResultContract()
-    ) {
-            res -> this.signInResult(res)
-    }
-
-
-
-    private fun signInResult(result : FirebaseAuthUIAuthenticationResult){
-        val response = result.idpResponse
-        if(result.resultCode == RESULT_OK){
-            user = FirebaseAuth.getInstance().currentUser
-        }else  {
-            Log.e( "ENTRY POINT ACTIVITY " , "ERROR IN LOGGGGGGING" + response?.error?.errorCode)
-        }
-    }*/
 }
 
 
@@ -111,8 +101,14 @@ private fun TeamsApp() {
     Scaffold(
         bottomBar = {NavBar(navController)}
     ) {
-    NavHost(navController, startDestination = NavigationKeys.Route.TEAMS_LIST) {
+    NavHost(navController, startDestination = NavigationKeys.Route.LOGIN) {
 
+        composable(route = NavigationKeys.Route.LOGIN){
+            LoginScreen(navController)
+        }
+        composable(route = NavigationKeys.Route.REGISTER){
+            RegisterScreen(navController)
+        }
         composable(route = NavigationKeys.Route.TEAMS_LIST) {
 
                 TeamsDestination(navController)
@@ -155,6 +151,322 @@ private fun TeamsApp() {
     }
     }
 }
+/*
+
+@Composable
+fun FormContainer(context : ComponentActivity){
+    val auth = Firebase.auth
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(8.dp)
+    ) {
+
+        val emailValue = remember { mutableStateOf(TextField(TextFieldValue() )) }
+        val passwordValue = remember { mutableStateOf(TextField( TextFieldValue() )) }
+
+
+        OutlinedTextField(
+            label =  { Text(text = "Email")},
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+            value =  emailValue.value,
+            singleLine = true,
+            modifier = Modifier.fillMaxWidth(),
+            onValueChange = {
+                emailValue.value = it
+            }
+        )
+
+        OutlinedTextField(
+            label =  { Text(text = "Password")},
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+            value =  passwordValue.value,
+            singleLine = true,
+            visualTransformation = PasswordVisualTransformation(),
+            modifier = Modifier.fillMaxWidth(),
+            onValueChange = {
+                passwordValue.value = it
+            }
+        )
+        Spacer(modifier = Modifier.padding(8.dp))
+
+        Button( modifier = Modifier.fillMaxWidth() ,
+            onClick = { auth.createUserWithEmailAndPassword(
+                 emailValue.value.text.trim(),
+                passwordValue.value.text.trim(),
+            ).addOnCompleteListener(context) {
+                task ->
+                if(task.isSuccessful){
+                    Log.d("AUTH" , "SUCCESS")
+                }else{
+                    Log.d("AUTH" , "Failed")
+                }
+            }
+
+            }) {
+            Text(text = "Register")
+        }
+    }
+}
+*/
+
+
+@Composable
+fun LoginScreen(navController: NavHostController) {
+    val auth = Firebase.auth
+    val context = LocalContext.current
+    val email = remember { mutableStateOf(TextFieldValue()) }
+    val emailErrorState = remember { mutableStateOf(false) }
+    val passwordErrorState = remember { mutableStateOf(false) }
+    val password = remember { mutableStateOf(TextFieldValue()) }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+    ) {
+        Text(text = buildAnnotatedString {
+            append("Connexion")
+        }, fontSize = 30.sp)
+        Spacer(Modifier.size(16.dp))
+        OutlinedTextField(
+            value = email.value,
+            onValueChange = {
+                if (emailErrorState.value) {
+                    emailErrorState.value = false
+                }
+                email.value = it
+            },
+            isError = emailErrorState.value,
+            modifier = Modifier.fillMaxWidth(),
+            label = {
+                Text(text = "Email*")
+            },
+        )
+        if (emailErrorState.value) {
+            Text(text = "Obligatoire")
+        }
+        Spacer(Modifier.size(16.dp))
+        val passwordVisibility = remember { mutableStateOf(true) }
+        OutlinedTextField(
+            value = password.value,
+            onValueChange = {
+                if (passwordErrorState.value) {
+                    passwordErrorState.value = false
+                }
+                password.value = it
+            },
+            isError = passwordErrorState.value,
+            modifier = Modifier.fillMaxWidth(),
+            label = {
+                Text(text = "Password*")
+            },
+        )
+        if (passwordErrorState.value) {
+            Text(text = "Obligatoire")
+        }
+        Spacer(Modifier.size(16.dp))
+        Button(
+            onClick = { auth.signInWithEmailAndPassword(
+                email.value.text.trim(),
+                password.value.text.trim(),
+            ).addOnCompleteListener { task ->
+                if(task.isSuccessful){
+                    navController.navigate(NavigationKeys.Route.TEAMS_LIST) {
+                        popUpTo(navController.graph.startDestinationId)
+                        launchSingleTop = true
+                    }}
+                }.addOnFailureListener{ exception ->
+                    Toast.makeText(
+                        context,
+                        "Erreur de connexion !!",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    Log.d("LOGIN" , "FAILED")
+            } }
+            ,
+            content = {
+                Text(text = "Connexion")
+            },
+            modifier = Modifier.fillMaxWidth(),
+        )
+        Spacer(Modifier.size(16.dp))
+        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
+            TextButton(onClick = {
+                navController.navigate(NavigationKeys.Route.REGISTER) {
+                    popUpTo(navController.graph.startDestinationId)
+                    launchSingleTop = true
+                }
+            }) {
+                Text(text = "S'enregistrer ?")
+            }
+        }
+    }
+}
+
+@Composable
+fun RegisterScreen(navController: NavHostController) {
+    val context = LocalContext.current
+    val name = remember {
+        mutableStateOf(TextFieldValue())
+    }
+    val auth = Firebase.auth
+    val email = remember { mutableStateOf(TextFieldValue()) }
+    val password = remember { mutableStateOf(TextFieldValue()) }
+    val confirmPassword = remember { mutableStateOf(TextFieldValue()) }
+
+    val nameErrorState = remember { mutableStateOf(false) }
+    val emailErrorState = remember { mutableStateOf(false) }
+    val passwordErrorState = remember { mutableStateOf(false) }
+    val confirmPasswordErrorState = remember { mutableStateOf(false) }
+    val scrollState = rememberScrollState()
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+            .verticalScroll(scrollState),
+        verticalArrangement = Arrangement.Center,
+    ) {
+
+        Text(text = buildAnnotatedString {
+                append("S'enregistrer")
+        }, fontSize = 30.sp)
+
+        Spacer(Modifier.size(16.dp))
+
+        OutlinedTextField(
+            value = email.value,
+            onValueChange = {
+                if (emailErrorState.value) {
+                    emailErrorState.value = false
+                }
+                email.value = it
+            },
+
+            modifier = Modifier.fillMaxWidth(),
+            isError = emailErrorState.value,
+            label = {
+                Text(text = "Email*")
+            },
+        )
+        if (emailErrorState.value) {
+            Text(text = "Obligatoire")
+        }
+        Spacer(modifier = Modifier.size(16.dp))
+
+        Spacer(Modifier.size(16.dp))
+        val passwordVisibility = remember { mutableStateOf(true) }
+        OutlinedTextField(
+            value = password.value,
+            onValueChange = {
+                if (passwordErrorState.value) {
+                    passwordErrorState.value = false
+                }
+                password.value = it
+            },
+            modifier = Modifier.fillMaxWidth(),
+            label = {
+                Text(text = "Password*")
+            },
+            isError = passwordErrorState.value,
+
+            visualTransformation = if (passwordVisibility.value) PasswordVisualTransformation() else VisualTransformation.None
+        )
+        if (passwordErrorState.value) {
+            Text(text = "Required")
+        }
+
+        Spacer(Modifier.size(16.dp))
+        val cPasswordVisibility = remember { mutableStateOf(true) }
+        OutlinedTextField(
+            value = confirmPassword.value,
+            onValueChange = {
+                if (confirmPasswordErrorState.value) {
+                    confirmPasswordErrorState.value = false
+                }
+                confirmPassword.value = it
+            },
+            modifier = Modifier.fillMaxWidth(),
+            isError = confirmPasswordErrorState.value,
+            label = {
+                Text(text = "Confirmer le  password*")
+            },
+            visualTransformation = if (cPasswordVisibility.value) PasswordVisualTransformation() else VisualTransformation.None
+        )
+        if (confirmPasswordErrorState.value) {
+            val msg = if (confirmPassword.value.text.isEmpty()) {
+                "Obligatoire"
+            } else if (confirmPassword.value.text != password.value.text) {
+                "Password ne match pas"
+            } else {
+                ""
+            }
+            Text(text = msg)
+        }
+        Spacer(Modifier.size(16.dp))
+        Button(
+            onClick = {
+                when {
+                    email.value.text.isEmpty() -> {
+                        emailErrorState.value = true
+                    }
+                    password.value.text.isEmpty() -> {
+                        passwordErrorState.value = true
+                    }
+                    confirmPassword.value.text.isEmpty() -> {
+                        confirmPasswordErrorState.value = true
+                    }
+                    confirmPassword.value.text != password.value.text -> {
+                        confirmPasswordErrorState.value = true
+                    }
+                    else -> {
+                        auth.createUserWithEmailAndPassword(
+                            email.value.text.trim(),
+                            password.value.text.trim(),
+                        ).addOnCompleteListener { task ->
+                            if(task.isSuccessful){
+                                Toast.makeText(
+                                    context,
+                                    "Enregistement Réussi !!",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                                navController.navigate(NavigationKeys.Route.TEAMS_LIST) {
+                                    popUpTo(navController.graph.startDestinationId)
+                                    launchSingleTop = true
+                                }
+                            }
+                        }.addOnFailureListener{ exception ->
+                            Toast.makeText(
+                                context,
+                                "Erreur de connexion !!",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                            Log.d("LOGIN" , "FAILED")
+                        }
+
+                    }
+                }
+            },
+            content = {
+                Text(text = "S'enregistrer")
+            },
+            modifier = Modifier.fillMaxWidth(),
+        )
+        Spacer(Modifier.size(16.dp))
+        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
+            TextButton(onClick = {
+                navController.navigate(NavigationKeys.Route.TEAMS_LIST) {
+                    popUpTo(navController.graph.startDestinationId)
+                    launchSingleTop = true
+                }
+            }) {
+                Text(text = "Connexion")
+            }
+        }
+    }
+}
+
 
 @Composable
 private fun TeamsDestination(navController: NavHostController) {
@@ -226,7 +538,8 @@ object NavigationKeys {
 
     object Route {
         const val PROFILE = "profile"
-
+        const val LOGIN = "login"
+        const val REGISTER = "register"
         // handle matches routes
         const val MATCHES_LIST = "matches_list"
         const val MATCH_DETAILS = "$MATCHES_LIST/{$MATCH_ID}"
---
+++ app/src/main/java/com/example/myapplication/ui/feature/auth/RegisterScreen.kt
@@ -0,0 +1,4 @@
+package com.example.myapplication.ui.feature.auth
+
+class RegisterScreen {
+}
